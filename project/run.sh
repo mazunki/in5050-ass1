09e78c4ff5e64464e142bbe77c158b281861c21a
Mazunki Hoksaas <rolferen@gmail.com>
2025-02-19 00:02:30 +0100

    more build tools

--- project/run.sh
+++ project/run.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-set -e
+set -eu
 
-PROJECT_ROOT="/home/in5050-g01/in5050-ass1/project"
+PROJECT_USER=in5050-g01
+PROJECT_ROOT="/home/${PROJECT_USER}/in5050-ass1/project"
 BUILD_DIR="${PROJECT_ROOT}/build"
 WORKDIR="${PROJECT_ROOT}/workdir"
-REMOTE_BUILD="in5050:${BUILD_DIR}"
-REMOTE_WORKDIR="in5050-gpu:${WORKDIR}"
 REPORT_FILE="report.nsys-rep"
 
-BUILDER="in5050"
-RUNNER="in5050-gpu"
+BUILDER="${PROJECT_USER}@in5050"
+RUNNER="${PROJECT_USER}@in5050-2016-10"
 
 cd "$(dirname "$0")"
 
@@ -25,24 +24,26 @@ runner() {
 
 pipeline() {
 	echo "[PIPELINE] Updating build server..."
-	rsync -av --progress . "${BUILDER}:${PROJECT_ROOT}/"
+	(set -x; rsync -av --progress . "${BUILDER}:${PROJECT_ROOT}/")
 
-	# echo "[PIPELINE] updating cmake..."
-	# builder "cd '${PROJECT_ROOT}' && rm -rf build && cmake -B build -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake"
+	echo "[PIPELINE] updating cmake..."
+	builder "cd '${PROJECT_ROOT}' && rm -rf build && cmake -B build -DCMAKE_TOOLCHAIN_FILE=in5050-toolchain.cmake"
 
 	echo "[PIPELINE] building project..."
 	builder "cd '${BUILD_DIR}' && make"
 
 	echo "[PIPELINE] syncing build machine with gpu machine..."
-	rsync -av --progress "${BUILDER}:${BUILD_DIR}/" "$RUNNER:${BUILD_DIR}/"
+	# (set -x; rsync -av --progress "${BUILDER}:${BUILD_DIR}/" "$RUNNER:${BUILD_DIR}/")
+	(set -x; ssh "${BUILDER}" "rsync -av --progress '${BUILD_DIR}/' '${RUNNER}:${BUILD_DIR}/'")
+	
 
 	echo "[PIPELINE] running profiling on gpu machine..."
 	cmd="${BUILD_DIR}/c63enc -h 288 -w 352 -o output -f 128 /home/in5050-g01/assets/foreman.yuv"
+	runner "rm -rf '${WORKDIR}' && mkdir '${WORKDIR}'"
 	runner "cd '${WORKDIR}' && nsys profile -o '${REPORT_FILE}' -- ${cmd}"
 
 	echo "[PIPELINE] Fetching profiling report..."
-	mkdir -p workdir
-	rsync -av --progress "$RUNNER:$WORKDIR/$REPORT_FILE" "workdir/${REPORT_FILE}"
+	(set -x; rsync -av --progress "$RUNNER:$WORKDIR/" "workdir/")
 }
 
 pipeline

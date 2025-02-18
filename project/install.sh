1e5b2d09723fc3c3e758e3b1134f98e6c187a308
Mazunki Hoksaas <rolferen@gmail.com>
2025-02-18 17:55:48 +0100

    test commit

--- project/install.sh
+++ project/install.sh
@@ -2,12 +2,14 @@
 set -e
 
 commit() {
+	echo commiting
 	git add src/ include/ CMakeLists.txt install.sh
 	git commit -m 'test commit'
 	git push
 }
 
 rebuild() {
+	echo rebuilding
 	pushd /home/in5050-g01/in5050-ass1/project/build
 	git pull
 	make
@@ -15,6 +17,7 @@ rebuild() {
 }
 
 deploy() {
+	echo deploying
 	pushd /home/in5050-g01/in5050-ass1/project/workdir
 	git pull
 	scp c63* in5050-2016-10:ass1/build/
@@ -22,13 +25,20 @@ deploy() {
 }
 
 profile() {
+	echo profiling
 	cd /home/in5050-g01/in5050-ass1/project/workdir
 	nsys profile -o report.nsys-rep -- ../build/c63enc -h 288 -w 352 -o output -f 128 /home/in5050-g01/assets/foreman.yuv
 }
 
 newreport() {
+	echo new report
+	echo remote in5050 rebuild deploy
 	ssh in5050 sh -c 'cd /home/in5050-g01/in5050-ass1/project && ./install.sh rebuild deploy'
+
+	echo remote in5050-gpu profile
 	ssh in5050-gpu sh -c 'cd /home/in5050-g01/in5050-ass1/project && ./install.sh profile'
+
+	echo getting report
 	scp in5050-gpu:/home/in5050-g01/ass1/workdir/report.nsys-rep .
 }
 

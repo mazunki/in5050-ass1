0e716a7f45e76863163812a64561f37252ebeeee
Mazunki Hoksaas <rolferen@gmail.com>
2025-02-18 17:38:23 +0100

    helper installer

---
+++ project/install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+commit() {
+	git add src/ include/
+	git commit -m 'test commit'
+	git push
+}
+
+rebuild() {
+	cd /home/in5050-g01/in5050-ass1/project/build
+	git pull
+	make
+}
+
+deploy() {
+	cd /home/in5050-g01/in5050-ass1/project/workdir
+	scp c63* in5050-2016-10:ass1/build/
+}
+
+profile() {
+	cd /home/in5050-g01/in5050-ass1/project/workdir
+	nsys profile -o report.nsys-rep -- ../build/c63enc -h 288 -w 352 -o output -f 128 /home/in5050-g01/assets/foreman.yuv
+}
+
+all() {
+	commit
+	ssh in5050 sh -c 'cd /home/in5050-g01/ass1/project && ./install.sh rebuild deploy'
+	ssh in5050-gpu sh -c 'cd /home/in5050-g01/ass1/project && ./install.sh profile'
+	scp in5050-gpu:/home/in5050-g01/ass1/workdir/report.nsys-rep .
+}
+
+for target; do
+	case "${target}" in
+		rebuild) rebuild ;;
+		deploy) deploy ;;
+		profile) profile ;;
+		all) all ;;
+		commit) commit ;;
+		*) exit 1 ;;
+	esac
+done
+
+

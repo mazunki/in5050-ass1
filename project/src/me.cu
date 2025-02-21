4c0c727a2989083ac77a63dc3e8b01fb19284853
Mazunki Hoksaas <rolferen@gmail.com>
2025-02-21 12:00:20 +0100

    style cleanup

--- project/src/me.cu
+++ project/src/me.cu
@@ -22,9 +22,7 @@
 __device__ static int sad_block_8x8(uint8_t *block1, uint8_t *block2, int stride)
 {
   int u, v;
-
   int result = 0;
-
   for (v = 0; v < MACROBLOCK_SIZE; ++v)
   {
     for (u = 0; u < MACROBLOCK_SIZE; ++u)
@@ -32,11 +30,10 @@ __device__ static int sad_block_8x8(uint8_t *block1, uint8_t *block2, int stride
       result += abs(block2[v*stride+u] - block1[v*stride+u]);
     }
   }
-
   return result;
 }
 
-/* Motion estimation for 8x8 block */
+/* Motion estimation for an 8x8 block */
 __device__ static void me_block_8x8(struct macroblock *mb, int mb_x, int mb_y,
     uint8_t *orig, uint8_t *ref, int padw, int padh, int range)
 {
@@ -48,20 +45,15 @@ __device__ static void me_block_8x8(struct macroblock *mb, int mb_x, int mb_y,
   int bottom = MIN(mb_y * MACROBLOCK_SIZE + range, padh - MACROBLOCK_SIZE);
 
   int x, y;
-
   int mx = mb_x * MACROBLOCK_SIZE;
   int my = mb_y * MACROBLOCK_SIZE;
-
   int best_sad = INT_MAX;
 
   for (y = top; y < bottom; ++y)
   {
     for (x = left; x < right; ++x)
     {
-      int sad = sad_block_8x8(orig + my*padw+mx, ref + y*padw+x, padw);
-
-      /* printf("(%4d,%4d) - %d\n", x, y, sad); */
-
+      int sad = sad_block_8x8(orig + my*padw + mx, ref + y*padw + x, padw);
       if (sad < best_sad)
       {
         mb->mv_x = x - mx;
@@ -83,8 +75,8 @@ __device__ static void me_block_8x8(struct macroblock *mb, int mb_x, int mb_y,
 __global__ void c63_motion_estimate_kernel(uint8_t *d_orig, uint8_t *d_recons, macroblock *d_mbs, int width, int height, int range) {
   int mb_x = blockIdx.x * blockDim.x + threadIdx.x;
   int mb_y = blockIdx.y * blockDim.y + threadIdx.y;
-  int mb_cols = width/MACROBLOCK_SIZE;
-  int mb_rows = height/MACROBLOCK_SIZE;
+  int mb_cols = width / MACROBLOCK_SIZE;
+  int mb_rows = height / MACROBLOCK_SIZE;
 
   if (mb_x >= mb_cols || mb_y >= mb_rows) {
     return;
@@ -98,9 +90,8 @@ __host__ void c63_motion_estimate(struct c63_common *cm)
 {
   /* Compare this frame with previous reconstructed frame */
   int range = cm->me_search_range;
-
   dim3 block_size(CUDA_THREADS_PER_BLOCK_X, CUDA_THREADS_PER_BLOCK_Y);
-  dim3 grid_size(cm->padw[Y_COMPONENT]/MACROBLOCK_SIZE, cm->padh[Y_COMPONENT]/MACROBLOCK_SIZE);
+  dim3 grid_size(cm->padw[Y_COMPONENT] / MACROBLOCK_SIZE, cm->padh[Y_COMPONENT] / MACROBLOCK_SIZE);
 
   /* Luma */
   c63_motion_estimate_kernel<<<grid_size, block_size>>>(cm->curframe->orig->Y, cm->refframe->recons->Y, cm->curframe->mbs[Y_COMPONENT], cm->padw[Y_COMPONENT], cm->padh[Y_COMPONENT], range);
@@ -117,7 +108,7 @@ static void mc_block_8x8(struct c63_common *cm, int mb_x, int mb_y,
     uint8_t *predicted, uint8_t *ref, int color_component)
 {
   struct macroblock *mb =
-    &cm->curframe->mbs[color_component][mb_y*cm->padw[color_component]/MACROBLOCK_SIZE+mb_x];
+    &cm->curframe->mbs[color_component][mb_y * cm->padw[color_component] / MACROBLOCK_SIZE + mb_x];
 
   if (!mb->use_mv) { return; }
 
@@ -125,17 +116,13 @@ static void mc_block_8x8(struct c63_common *cm, int mb_x, int mb_y,
   int top = mb_y * MACROBLOCK_SIZE;
   int right = left + MACROBLOCK_SIZE;
   int bottom = top + MACROBLOCK_SIZE;
-
   int w = cm->padw[color_component];
 
-  /* Copy block from ref mandated by MV */
-  int x, y;
-
-  for (y = top; y < bottom; ++y)
+  for (int y = top; y < bottom; ++y)
   {
-    for (x = left; x < right; ++x)
+    for (int x = left; x < right; ++x)
     {
-      predicted[y*w+x] = ref[(y + mb->mv_y) * w + (x + mb->mv_x)];
+      predicted[y * w + x] = ref[(y + mb->mv_y) * w + (x + mb->mv_x)];
     }
   }
 }
@@ -143,7 +130,6 @@ static void mc_block_8x8(struct c63_common *cm, int mb_x, int mb_y,
 void c63_motion_compensate(struct c63_common *cm)
 {
   int mb_x, mb_y;
-
   /* Luma */
   for (mb_y = 0; mb_y < cm->mb_rows; ++mb_y)
   {
@@ -153,7 +139,6 @@ void c63_motion_compensate(struct c63_common *cm)
           cm->refframe->recons->Y, Y_COMPONENT);
     }
   }
-
   /* Chroma */
   for (mb_y = 0; mb_y < cm->mb_rows / 2; ++mb_y)
   {
@@ -166,3 +151,4 @@ void c63_motion_compensate(struct c63_common *cm)
     }
   }
 }
+
